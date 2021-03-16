56fadfb31b7ffe2feb8be2c0f667d05de4947f47
wilmergarcia7 <61239560+wilmergarcia7@users.noreply.github.com>
2021-03-15 18:34:50 -0600

    Se agrego firebase
    Se agrego firebase

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -4,26 +4,48 @@ import NewTodo from "./NewTodo";
 import TodoList from "./TodoList";
 import "./TodoList.css";
 
+import firebaseSDK from '../../../FireBaseInit';
+
 function Todo(){
   const [todoData, setTodoData] = useState({
     todos:[],
     newTodo:"",
   });
+
+  useEffect(
+    ()=>{
+      const todosRef = firebaseSDK.database().ref('todos').orderByKey().limitToLast(100);
+      todosRef.on('child_added', snapshot => {
+        let newTodo = { ...snapshot.val(), fb_id: snapshot.key };
+        let newTodos = todoData.todos;
+        newTodos.push(newTodo);
+        setTodoData({...todoData, todos: newTodos});
+      })
+    },
+    []
+  );
+
+
   const onChange = (e)=>{
     const {name, value} = e.currentTarget;
     setTodoData({...todoData, newTodo: value});
   };
+
   const onAddNew = (e)=>{
     let newToo = {
       description: todoData.newTodo,
       completed:false,
       id : new Date().getTime()
     };
+    firebaseSDK.database().ref("todos").push(newToo);
+    /*
     let newTodos = todoData.todos;
     newTodos.push(newToo);
 
     setTodoData({todos:newTodos, newTodo: ""});
+    */
   }
+
   const doneHandler = (id)=>{
     const newTodos = todoData.todos.map((o)=>{
       if(o.id == id){
@@ -34,6 +56,7 @@ function Todo(){
 
     setTodoData({...todoData, todos:newTodos});
   };
+
   const deleteHandler = (id)=>{
     const newTodos = todoData.todos.filter((o) => {
       return o.id !==id;
@@ -41,7 +64,9 @@ function Todo(){
 
     setTodoData({ ...todoData, todos: newTodos });
   }
+
   const tmpTodos = todoData.todos.map( (o)=>{return JSON.stringify(o)} ).join(" | ");
+
   return (
     <section>
      <NewTodo
